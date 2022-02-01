dae03c4aad13f1ecd0300496f5a6e3f1706cdc52
Noaha811 <nanderson95@students.mchenry.edu>
2022-01-31 18:40:03 -0600

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+//Noah Anderson
+fun main() {
+    //Initialize array of ingredients
+    val itemsArray = arrayOf("Buns", "Ground Beef", "Lettuce", "Cheese", "Tomato",
+                            "Onion", "Pickles", "Condiments")
+
+    //print out each element in itemsArray using a for:each loop
+    println("These ingredients are commonly found on a cheeseburger")
+    for (x in itemsArray){
+        println(x)
+    }
+
+}
